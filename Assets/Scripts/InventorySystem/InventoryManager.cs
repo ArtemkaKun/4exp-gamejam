c2708e13237ed64685f956c086428513368ff00a
Artem Yurchenko <zumikq78@gmail.com>
2022-02-26 07:54:45 +0100

    InventoryManager class - updating inventory UI after changes logic was implemented

--- Assets/Scripts/InventorySystem/InventoryManager.cs
+++ Assets/Scripts/InventorySystem/InventoryManager.cs
@@ -5,21 +5,27 @@ namespace InventorySystem
 {
 	public class InventoryManager : MonoBehaviour
 	{
+		[field: SerializeField]
+		private InventoryView InventoryView { get; set; }
+		
 		private List<InventoryObject> Items { get; } = new List<InventoryObject>();
 
 		public void AddItem (InventoryObject item)
 		{
 			Items.Add(item);
+			InventoryView.UpdateInventory(Items);
 		}
 		
 		public void RemoveItem (InventoryObject item)
 		{
 			Items.Remove(item);
+			InventoryView.UpdateInventory(Items);
 		}
 
 		public void RemoveAllItems ()
 		{
 			Items.Clear();
+			InventoryView.UpdateInventory(Items);
 		}
 	}
 }
