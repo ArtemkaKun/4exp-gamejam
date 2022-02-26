1c474a3dccb25d678ea36802c4e556d9dcbeb3b0
Jakub Miętus <jakub.mietus@4experience.co>
2022-02-26 12:34:13 +0100

    Create random names generator

--- Assets/Scripts/NamesGenerator/NamesGenerator.cs
+++ Assets/Scripts/NamesGenerator/NamesGenerator.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class NamesGenerator : MonoBehaviour
 {
+	[field: SerializeField]
+	private List<string> NamesCollection { get; set; } = new List<string>();
 
+	public string GetRandomName ()
+	{
+		return NamesCollection.Count > 0 ? NamesCollection[Random.Range(0, NamesCollection.Count - 1)] : string.Empty;
+	}
 }
