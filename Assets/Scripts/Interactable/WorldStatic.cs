f05362126961f25a1c9af1d22d5910612747efa8
Artem Yurchenko <zumikq78@gmail.com>
2022-02-26 10:42:51 +0100

    WorldStatic class - calling IncreaseLeftTime method was implemented

--- Assets/Scripts/Interactable/WorldStatic.cs
+++ Assets/Scripts/Interactable/WorldStatic.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TimerSystem;
 using UnityEngine;
 
 public class WorldStatic : MonoBehaviour
 {
+	[field: SerializeField]
+	private int IncreasePlayerLifeInSeconds { get; set; }
+	
     protected virtual void OnEnable()
     {
         BoostPlayer();
@@ -11,6 +15,6 @@ public class WorldStatic : MonoBehaviour
 
     private void BoostPlayer()
     {
-
-    }
+		FindObjectOfType<Timer>().IncreaseLeftTime(IncreasePlayerLifeInSeconds);
+	}
 }
