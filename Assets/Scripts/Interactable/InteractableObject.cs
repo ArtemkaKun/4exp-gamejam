ab127219ebe9afe19f6a610af2b1e8891a8f297b
Jakub Miętus <jakub.mietus@4experience.co>
2022-02-26 02:23:16 +0100

    Enabling other interactable when interacted

--- Assets/Scripts/Interactable/InteractableObject.cs
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -7,9 +7,42 @@ public class InteractableObject : MonoBehaviour
 {
     [field: SerializeField]
     public UnityEvent OnInteract { get; private set; }
+    [field: SerializeField]
+    private GameObject ObjectToDisplay { get; set; }
+    [field: SerializeField]
+    private List<GameObject> InteractableObjectsToSpawn { get; set; }
 
     public void PlayOpenAnimation(bool isPlaying)
     {
         Debug.Log(isPlaying ? "gramnko" : " nie");
+    }
+
+    protected virtual void OnEnable()
+    {
+        OnInteract.AddListener(OnInteractEventHandler);
+    }
+
+    protected virtual void OnDisable()
+    {
+        OnInteract.RemoveListener(OnInteractEventHandler);
+    }
+
+    private void OnInteractEventHandler()
+    {
+        SpawnObjects();
+        DestroyThisObject();
+    }
+
+    private void SpawnObjects()
+    {
+        foreach (GameObject interactable in InteractableObjectsToSpawn)
+        {
+            interactable.SetActive(true);
+        }
+    }
+
+    private void DestroyThisObject()
+    {
+        Destroy(gameObject);
     }
 }
